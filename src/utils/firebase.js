33c7dc2311a9f571c7c9611b0c001fa7b8b459e9
n8220112 <n8220112@naver.com>
2025-07-28 11:53:19 +0900

    first commit

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,13 +3,13 @@ import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXLCkmV97EhXjGduVephObC9WlSz2gCd0",
-  authDomain: "diary-project-2456c.firebaseapp.com",
-  projectId: "diary-project-2456c",
-  storageBucket: "diary-project-2456c.firebasestorage.app",
-  messagingSenderId: "890003534712",
-  appId: "1:890003534712:web:8c4062edaa4769a3a40e8d",
-  measurementId: "G-L9X0ZMTLKE",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 const app = initializeApp(firebaseConfig);
 
